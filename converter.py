0b5701605953bcb1aa72a83d0cda54780446c715
FlyingNils <nilsbriggen@gmail.com>
2023-10-23 21:16:34 +0200

    content update

---
+++ converter.py
@@ -0,0 +1,11 @@
+import json
+
+def convert_to_python():
+    with open("all-content.json", "r", encoding='utf-8-sig') as f:
+        content = json.load(f, )
+    return content
+
+content = convert_to_python()
+
+with open("all-content.py", "w") as f:
+    f.write("data = " + str(content))
